7def83eeacfc04c470e21b949a4007020ece09a6
Pedro Henrique <127359574+Phhenrique3@users.noreply.github.com>
2025-09-22 21:23:53 -0300

    crud_aluno + h2

--- src/main/java/com/example/crud_aluno/controller/alunoContorller.java
+++ src/main/java/com/example/crud_aluno/controller/alunoContorller.java
@@ -2,6 +2,8 @@ package com.example.crud_aluno.controller;
 
 import com.example.crud_aluno.model.Aluno;
 import com.example.crud_aluno.repository.AlunoRepository;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,21 +12,45 @@ import java.util.List;
 @RequestMapping("/alunos")
 public class alunoContorller {
 
-private  final AlunoRepository alunoRepository;
+    private final AlunoRepository alunoRepository;
 
-public alunoContorller(AlunoRepository alunoRepository) {
-    this.alunoRepository = alunoRepository;
-}
+    public alunoContorller(AlunoRepository alunoRepository) {
+        this.alunoRepository = alunoRepository;
+    }
 
-@GetMapping
-    public List<Aluno> listarAlunos(){
-    return alunoRepository.findAll();
+    @GetMapping
+    public List<Aluno> listarAlunos() {
+        return alunoRepository.findAll();
     }
 
     @PostMapping
     public Aluno save(@RequestBody Aluno aluno) {
-    return alunoRepository.save(aluno);
-}
+        return alunoRepository.save(aluno);
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Aluno> atualizarAluno(@PathVariable Long id, @RequestBody Aluno alunoAtualizado) {
+        return alunoRepository.findById(id).map(aluno -> {
+            aluno.setNome(alunoAtualizado.getNome());
+            aluno.setEmail(alunoAtualizado.getEmail());
+            aluno.setCpf(alunoAtualizado.getCpf());
+            aluno.setCpf(alunoAtualizado.getCpf());
+            return ResponseEntity.ok(alunoRepository.save(aluno));
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletarAluno(@PathVariable Long id) {
+        return alunoRepository.findById(id)
+                .map(aluno -> {
+                    alunoRepository.deleteById(id);
+                    return ResponseEntity.ok().body(
+                            "Aluno com ID " + id + " foi excluído com sucesso."
+                    );
+                })
+                .orElse(ResponseEntity.status(404).body(
+                        "Aluno com ID " + id + " não encontrado."
+                ));
+    }
 
 }
